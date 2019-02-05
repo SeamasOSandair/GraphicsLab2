0ee487b29db11e4c5d5846eeb475474ed490d556
Unknown <o_sandair-s@ulster.ac.uk>
2019-02-05 12:05:33 +0000

    Partially Complete

---
+++ GraphicsLab2/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h used to store standard and project specific header files
+// represents the start of the compile chain
+
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include "d3dx12.h"
+#include <stdio.h>
